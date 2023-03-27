f62824b9914551722515001dd4ff5c5d49b390c0
Mustapha Abourar <elmustapha.abourar@outlook.com>
2023-03-27 11:31:43 +0200

    markdown to html

--- markdown2html.py
+++ markdown2html.py
@@ -6,19 +6,25 @@ import sys
 import os
 import markdown
 
-if len(sys.argv) < 2:
+# Check for correct number of arguments
+if len(sys.argv) < 3:
     print("Usage: markdown2html.py README.md README.html")
     sys.exit(1)
 
+# Check if file exists
 inputFile = sys.argv[1]
 outputFile = sys.argv[2]
 
+# Check if file exists
 if not os.path.exists(inputFile):
     print(f"Usage: ./markdown2html.py README.md README.html")
     sys.exit(1)
 
+# Convert markdown to html
 with open(inputFile) as f:
     html_txt = markdown.markdown(f.read())
+# Write html to file
 with open(outputFile, "w") as f:
     f.write(html_txt)
+# Exit
 sys.exit(0)
