de10b2cb8be7729b11b6c6c126ec2b7b783922d6
Mustapha Abourar <elmustapha.abourar@outlook.com>
2023-03-27 11:27:31 +0200

    markdown to html

---
+++ markdown2html.py
@@ -0,0 +1,21 @@
+#!/usr/bin/pyhton3
+import sys
+import os
+import markdown
+
+if len(sys.argv) < 2:
+    print("Usage: markdown2html.py README.md README.html")
+    sys.exit(1)
+
+inputFile = sys.argv[1]
+outputFile = sys.argv[2]
+
+if not os.path.exists(inputFile):
+    print(f"Usage: ./markdown2html.py README.md README.html")
+    sys.exit(1)
+
+with open(inputFile) as f:
+    html_txt = markdown.markdown(f.read())
+with open(outputFile, "w") as f:
+    f.write(html_txt)
+sys.exit(0)
