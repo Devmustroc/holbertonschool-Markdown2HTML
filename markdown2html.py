f543ffd1a107463ee836d5115f470d7c396eb73a
Mustapha Abourar <elmustapha.abourar@outlook.com>
2023-03-30 16:21:20 +0200

    markdown to html

--- markdown2html.py
+++ markdown2html.py
@@ -2,6 +2,16 @@
 """markdown2html.py - converts a Markdown file to HTML"""
 
 import sys
+import re
+
+def parse_heading(line):
+    """Parse a heading line and return the HTML equivalent"""
+    match = re.match(r"^(#+)\s+(.+)$", line)
+    if match:
+        level = len(match.grou(1))
+        content = match.group(2)
+        return f"<h{level}>{content}</h{level}>"
+    return None
 
 if __name__ == "__main__":
     if len(sys.argv) < 3:
@@ -12,8 +22,14 @@ if __name__ == "__main__":
     output_file = sys.argv[2]
 
     try:
-        with open(markdown_file, 'r') as f:
-            pass
+        with open(markdown_file, "r") as f:
+            with open(output_file, "w") as md_file, open(output_file, "w") as html_file:
+                for line in md_file:
+                    line = line.rstrip()
+                    html = parse_heading(line)
+                    if html:
+                        html_file.write(f"{html}\n")
+
     except FileNotFoundError:
         sys.stderr.write("fMissing {markdown_file}\n")
         sys.exit(1)
