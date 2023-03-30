cae9dde0d88b48e9b519c263e126df999ccb3a7f
Mustapha Abourar <101810866+Devmustroc@users.noreply.github.com>
2023-03-30 16:04:36 +0200

    nu project

---
+++ markdown2html.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""markdown2html.py - converts a Markdown file to HTML"""
+
+import sys
+
+if __name__ == "__main__":
+    if len(sys.argv) < 3:
+        sys.stderr.write("Usage: ./markdown2html.py README.md README.html\n")
+        sys.exit(1)
+
+    markdown_file = sys.argv[1]
+    output_file = sys.argv[2]
+
+    try:
+        with open(markdown_file, 'r') as f:
+            pass
+    except FileNotFoundError:
+        sys.stderr.write("Missing {}\n".format(markdown_file))
+        sys.exit(1)
+
+    sys.exit(0)
