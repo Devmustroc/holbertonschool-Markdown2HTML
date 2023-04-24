a1800a26ccf50a31087e5a1119a47781a839e7d1
Mustapha Abourar <101810866+Devmustroc@users.noreply.github.com>
2023-04-24 18:42:03 +0200

    nu project

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-## markdown to Html
+# My html title 1
+## My html title 2
+### My html title 3
+#### My html title 4
+##### My html title 5
+###### My html title 6
--- markdown2html.py
+++ markdown2html.py
@@ -1,21 +1,55 @@
 #!/usr/bin/python3
 """markdown2html.py - converts a Markdown file to HTML"""
 
-import sys
+from sys import argv, stderr, exit
+import os
 
-if __name__ == "__main__":
-    if len(sys.argv) < 3:
-        sys.stderr.write("Usage: ./markdown2html.py README.md README.html\n")
-        sys.exit(1)
 
-    markdown_file = sys.argv[1]
-    output_file = sys.argv[2]
+def input_check():
+    """Checks input"""
+    if len(argv) != 3:  # check for correct number of arguments
+        print("Usage: ./markdown2html.py README.md README.html", file=stderr)  # print error
+        exit(1)  # exit
+    if not os.path.exists(argv[1]):  # check if file exists
+        print("Missing {}".format(argv[1]), file=stderr)  # print error
+        exit(1)  # exit
 
-    try:
-        with open(markdown_file, 'r') as f:
-            pass
-    except FileNotFoundError:
-        sys.stderr.write("Missing {}\n".format(markdown_file))
-        sys.exit(1)
 
-    sys.exit(0)
+def file_to_array():
+    """Converts file to array"""
+    with open(argv[1], 'r') as file:  # open file
+        array = [line for line in file]  # create array
+    return array  # return array
+
+
+def array_parser(array):
+    """Parses array"""
+    result = ""  # result string
+    i = 0  # index
+    while i < len(array):  # loop through array
+        if '#' in array[i]:  # if line is a header
+            h_level = array[i].count('#')  # count number of #
+            text = array[i].strip('#\n').strip()  # get text
+            html = f"<h{h_level}>{text}</h{h_level}>"  # create html
+            result += html + "\n"  # add to result
+        i += 1
+    return result
+
+
+def file_writer(result, html_file):
+    """Writes result to file"""
+    with open(argv[2], 'w') as file:  # open file
+        file.write(result)  # write result
+    exit(0)  # exit
+
+
+def main():  # main function
+    """Main function"""
+    input_check()  # check input
+    array = file_to_array()  # convert file to array
+    text = array_parser(array)
+    file_writer(text, argv[2])  # parse array
+
+
+if __name__ == "__main__":  # if file is run directly
+    main()  # run main function
